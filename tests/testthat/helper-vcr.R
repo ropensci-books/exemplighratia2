a4552991fd211632b4e6ec9aa3ba9e5e38afec90
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-09-09 12:54:06 +0200

    useless

--- tests/testthat/helper-vcr.R
+++ tests/testthat/helper-vcr.R
@@ -8,10 +8,3 @@ if (!gh::gh_token_exists()) {
     stop("No API key nor cassettes, tests cannot be run.", call. = FALSE)
   }
 }
-
-invisible(vcr::vcr_configure(
-  dir = vcr_dir,
-  # Filter the request header where the token is sent, make sure you know
-  # how authentication works in your case and read the Security chapter :-)
-  filter_request_headers = list(Authorization = "My bearer token is safe")
-))
